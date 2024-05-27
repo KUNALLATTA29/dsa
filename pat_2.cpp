e53180eccaf919a25933b706123fdf6031f29d2b
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-05-27 18:03:30 +0530

    Add files via upload

---
+++ pat_2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    for(int i=0;i<5;i++){
+        for(int j=i;j<5;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ pat_3.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
