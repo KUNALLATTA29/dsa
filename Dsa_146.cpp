84347cc378473c259b1a6c46bda4eb229202f644
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-06 22:15:41 +0530

    Dsa_146.cpp

---
+++ Dsa_146.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    reverse(str.begin(), str.end());
+    cout << "Reversed string: " << str << endl;
+    return 0;
+}
