58d1eb6da4bcb48912e16ce2286dfd4c9c517076
KUNALLATTA29 <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-07 22:52:12 +0530

    Add files via upload

---
+++ dsa_148.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binary(vector<int> arr, int s, int e, int k){
+    int res = -1;
+    while(s<=e){
+        int mid = (s+e)/2;
+        if(arr[mid]==k){
+            res = mid;
+            s = mid+1;
+        }else if(arr[mid]>k){
+            e=mid-1;
+        }else if(arr[mid]<k){
+            s=mid+1;
+        }
+    }
+    return res;
+}
+
+int main(){
+    
+    vector<int> arr = {1,2,4,4,4,4,4,4,7,8,9,11};
+    int k;
+    cin>>k;
+    int n = arr.size();
+    int index = binary(arr,0,n-1,k);
+    cout<<index;
+    
+    return 0;
+}
