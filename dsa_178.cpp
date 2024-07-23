dcaa182f115c5ca3ec0c268d39a82b56ee0fea03
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-23 21:34:19 +0530

    dsa_178.cpp

---
+++ dsa_178.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector<int> arr = {1,2,3,4,5};
+    reverse(arr.begin(),arr.end());
+    
+    for(auto it:arr){
+        cout<<it<<" ";
+    }
+    
+    return 0;      
+}
