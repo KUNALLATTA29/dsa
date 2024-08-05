fad983f5f379454974346643429d2a21ffe7c993
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-08-05 22:57:10 +0530

    dsa_190.cpp

---
+++ dsa_190.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    reverseArray(arr, size);
+
+    cout << "Reversed array: ";
+    printArray(arr, size);
+
+    return 0;
+}
