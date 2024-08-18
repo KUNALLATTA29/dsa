06ade01eddfcd2d9fcb939eace83df2ae9cba40d
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-08-18 23:13:58 +0530

    Add files via upload

--- dsa_191.cpp
+++ dsa_191.cpp
@@ -1,22 +1,58 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void printSubarrays(const vector<int>& arr) {
-    int n = arr.size();
-    for (int start = 0; start < n; ++start) {
-        for (int end = start; end < n; ++end) {
-            for (int k = start; k <= end; ++k) {
-                cout << arr[k] << " ";
-            }
-            cout << endl;
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+    
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+    
+    void display() {
+        Node* current = head;
+        while (current) {
+            cout << current->data << " -> ";
+            current = current->next;
+        }
+        cout << "nullptr" << endl;
+    }
+    
+    ~LinkedList() {
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
         }
     }
-}
+};
 
 int main() {
-    vector<int> arr = {1, 2, 3, 4};
-    printSubarrays(arr);
+    LinkedList list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.display();
     return 0;
 }
